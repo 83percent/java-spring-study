30a4c9d4ef908fd92edb01fdf9aea27f62ae2947
JaeHoon Lee <hoonni2709@naver.com>
2022-02-13 22:47:26 +0900

    Annotation Study

--- src/main/java/com/entity/NewlecExam.java
+++ src/main/java/com/entity/NewlecExam.java
@@ -1,8 +1,27 @@
 package com.entity;
 
+import org.springframework.stereotype.Component;
+
+@Component("exam2")
 public class NewlecExam implements Exam {
     private int kor, eng, com, math;
 
+    public void setCom(int com) {
+        this.com = com;
+    }
+
+    public void setEng(int eng) {
+        this.eng = eng;
+    }
+
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+    
     @Override
     public int total() {
         return kor + eng + com + math;
--- src/main/java/com/ui/GridExamConsole.java
+++ src/main/java/com/ui/GridExamConsole.java
@@ -2,12 +2,21 @@ package com.ui;
 
 import com.entity.Exam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GridExamConsole implements ExamConsole {
     private Exam exam;
+
+    @Autowired
+    @Qualifier("exam2")
     @Override
     public void setExam(Exam exam) {
-        this.exam = exam;        
+        this.exam = exam;
     }
+
     @Override
     public void print() {
         System.out.println("|   total   |   avg   |");
